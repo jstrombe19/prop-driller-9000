ee38187c8f7280af8cb3d0b26e054ea8b0f7ba56
github username <github email address>
2019-10-29 00:16:44 -0600

    Removed all props from components at the App.js level.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import './App.css';
-import ProptimusOmega from './proptimusOmega.js';
-import ProptimusBeta from './proptimusBeta.js';
-import ProptimusPrime from './proptimusPrime.js';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -14,44 +11,24 @@ export default class App extends React.Component {
         "Proptimus Beta": "https://library.kissclipart.com/20181004/jbw/kissclipart-alola-exeggutor-height-clipart-optimus-prime-trans-6afe73e9efab0cac.png",
         "Proptimus Omega": "https://cdn.shopify.com/s/files/1/0076/4103/8906/products/Transformers-Movie-Studio-Series-38-Voyager-G1-Optimus-Prime-Semi-truck-Render_1024x1024.png?v=1549582726",
       },
-      drilledProp1: "I am the last drilled prop - Proptimus Prime!!!",
-      drilledProp2: "I am the middle drilled prop - Proptimus Beta!",
-      drilledProp3: "I am the first drilled prop - Proptimus Omega...",
-      activeProp: ""
     }
   }
 
 
   callForBadHelp = (event) => {
-    event.preventDefault();
-    console.log("Bad help has arrived!")
-    this.setState({
-      activeProp: "Proptimus Omega"
-    });
+
   }
 
   callForBetterHelp = (event) => {
-    event.preventDefault();
-    console.log("Better help has arrived.")
-    this.setState({
-      activeProp: "Proptimus Beta"
-    })
+
   }
 
   gimmeAllYouGot = (event) => {
-    event.preventDefault();
-    console.log("It's time to kick brass!")
-    this.setState({
-      activeProp: "Proptimus Prime"
-    })
+
   }
 
   sendThemHome = (event) => {
-    event.preventDefault();
-    console.log("I am Proptimus Prime, and I send this message to any surviving Proptobots taking refuge among the state: We are here. We are waiting.")
-    this.setState({
-      activeProp: ""
-    })
+
   }
 
   render() {
@@ -59,44 +36,20 @@ export default class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img
-            src={this.state.callForHelp}
+            src={}
             className="App-logo"
             alt="logo"
-            onClick={this.callForBadHelp}
           />
 
-          {this.state && this.state.activeProp === "Proptimus Omega" &&
+          {this.state && this.state.activeProp === "" /*Which Proptobot should be checked for?*/ &&
           <>
-            <p className="proptimus-omega-text">Will this do, human?</p>
-            <ProptimusOmega
-              proptimusPhotos={this.state.proptimusPhotos}
-              catchPhrase={this.state.drilledProp3}
-              callForBetterHelp={this.callForBetterHelp}
+            <p className="">Will this do, human?</p>
+            <Proptimus /*Specify your Proptobot here.*/
+              /*You need to pass all of your props to this component here.*/
             />
           </>
           }
 
-          {this.state && this.state.activeProp === "Proptimus Beta" &&
-          <>
-            <p className="proptimus-beta-text">Are you sure this situation warrants this?</p>
-            <ProptimusBeta
-              proptimusPhotos={this.state.proptimusPhotos}
-              catchPhrase={this.state.drilledProp2}
-              sendInTheCavalry={this.gimmeAllYouGot}
-            />
-          </>
-          }
-
-          {this.state && this.state.activeProp === "Proptimus Prime" &&
-          <>
-            <p className="proptimus-prime-text">There are no bigger guns!</p>
-            <ProptimusPrime
-              proptimusPhotos={this.state.proptimusPhotos}
-              catchPhrase={this.state.drilledProp1}
-              situationResolved={this.sendThemHome}
-            />
-          </>
-          }
         </header>
       </div>
     );
